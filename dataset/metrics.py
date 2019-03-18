563d2e0fc95f2e04d063ee5493cf9d6c14c7183d
Nelson <ennauata@gmail.com>
2019-03-17 19:27:15 -0700

    added loop metric

--- dataset/metrics.py
+++ dataset/metrics.py
@@ -37,10 +37,10 @@ class Metrics():
         precision = self.curr_edge_tp/(self.curr_edge_tp+self.curr_edge_fp+1e-8)
         return recall, precision
 
-    # def calc_loop_metrics(self):
-    #     recall = self.curr_loop_tp/self.n_loop_samples
-    #     precision = self.curr_loop_tp/(self.curr_loop_tp+self.curr_loop_fp+1e-8)
-    #     return recall, precision
+    def calc_loop_metrics(self):
+        recall = self.curr_loop_tp/self.n_loop_samples
+        precision = self.curr_loop_tp/(self.curr_loop_tp+self.curr_loop_fp+1e-8)
+        return recall, precision
 
     def print_metrics(self):
 
@@ -54,9 +54,9 @@ class Metrics():
         f_score = 2.0*precision*recall/(precision+recall+1e-8)
         print('Overall Scores\n-- edges \nrecall: %.3f\nprecision: %.3f\nf_score: %.3f\n' % (recall, precision, f_score))
 
-        # recall, precision = self.calc_loop_metrics()
-        # f_score = 2.0*precision*recall/(precision+recall+1e-8)
-        # print('-- loops \nrecall: %.3f\nprecision: %.3f\nf_score: %.3f\n' % (recall, precision, f_score))
+        recall, precision = self.calc_loop_metrics()
+        f_score = 2.0*precision*recall/(precision+recall+1e-8)
+        print('-- loops \nrecall: %.3f\nprecision: %.3f\nf_score: %.3f\n' % (recall, precision, f_score))
 
         # print('Per sample')
         # for k in self.per_sample_score.keys():
@@ -86,70 +86,11 @@ class Metrics():
         self.per_loop_sample_score = {}
         return
 
-    # def forward_loops(self, building, edges_pred, thresh=0.8):
+    def forward(self, building, thresh=4.0, iou_thresh=0.8):
 
-    #     edges_pred[edges_pred>0.5] = 1.0	
-    #     edges_pred[edges_pred<=0.5] = 0.0
-
-    #     ## Use annots here for kinds of corners
-    #     annots_edges_state = np.ones(building.edge_corner_annots.shape[0]) # dummy vector all 1's
-    #     corners_annot = np.array(building.corners_annot)[:, :2]
-    #     gt_loops = np.array(building.find_loops(annots_edges_state, building.edge_corner_annots, corners_annot, corners_annot.shape[0])) 
-    #     pred_loops = np.array(building.find_loops(edges_pred, building.edge_corner, building.corners_det, building.corners_det.shape[0]))
-
-    #     per_sample_tp = 0.0
-    #     per_sample_fp = 0.0
-    #     found = [False] * gt_loops.shape[0]
-
-    #     for pred_l in pred_loops:
-
-    #         # get closest gt
-    #         near_gt = [0, 0, None]
-    #         pred_loop_im = Image.new("L", (256, 256))
-    #         draw = ImageDraw.Draw(pred_loop_im)
-    #         loop_coords= [(x, y) for (y, x) in pred_l]
-    #         draw.polygon(loop_coords, fill='white')
-
-    #         for k, gt_l in enumerate(gt_loops):
-    #             gt_loop_im = Image.new("L", (256, 256))
-    #             draw = ImageDraw.Draw(gt_loop_im)
-    #             loop_coords= [(x, y) for (y, x) in gt_l]
-    #             draw.polygon(loop_coords, fill='white')
-    #             iou = np.logical_and(gt_loop_im, pred_loop_im).sum()/np.logical_or(gt_loop_im, pred_loop_im).sum()
-
-    #             # print(iou)
-    #             # plt.figure()
-    #             # plt.imshow(pred_loop_im)
-    #             # plt.figure()
-    #             # plt.imshow(gt_loop_im)
-    #             # plt.show()
-
-    #             # hit (>= thresh) and not found yet
-    #             if iou >= near_gt[1]:   
-    #                 near_gt = [k, iou, gt_l]
-
-    #         # hit (<= thresh) and not found yet 
-    #         if near_gt[1] >= thresh and not found[near_gt[0]]:
-    #             per_sample_tp += 1.0
-    #             found[near_gt[0]] = True
-
-    #         # not hit or already found
-    #         else:
-    #             per_sample_fp += 1.0
-
-    #     # update counters
-    #     self.curr_loop_tp += per_sample_tp
-    #     self.curr_loop_fp += per_sample_fp
-    #     self.n_loop_samples += gt_loops.shape[0]
-    #     self.per_corner_sample_score.update({building._id: {'recall': per_sample_tp/gt_loops.shape[0], 'precision': per_sample_tp/(per_sample_tp+per_sample_fp+1e-8)}}) 
-        
-    #     return
-
-    def forward(self, building, thresh=4.0):
-
+        ## Compute corners precision/recall
         gts = np.array(building.corners_annot)[:, :2]
         dets = building.corners_det
-        
         per_sample_corner_tp = 0.0
         per_sample_corner_fp = 0.0
         found = [False] * gts.shape[0]
@@ -181,13 +122,11 @@ class Metrics():
         self.n_corner_samples += gts.shape[0]
         self.per_corner_sample_score.update({building._id: {'recall': per_sample_corner_tp/gts.shape[0], 'precision': per_sample_corner_tp/(per_sample_corner_tp+per_sample_corner_fp+1e-8)}}) 
 
-        # for each edge detection
-        #edge_list_pred = np.array(edge_list_pred)
+        ## Compute edges precision/recall
         per_sample_edge_tp = 0.0
         per_sample_edge_fp = 0.0
 
         # for each detected edge
-        #for l, e_det in enumerate(edge_list_pred):
         for l, e_det in enumerate(building.edge_corner):
             c1, c2 = e_det
             
@@ -211,44 +150,102 @@ class Metrics():
             else:
                 per_sample_edge_fp += 1.0
 
-        # update counters for corners
+        # update counters for edges
         self.curr_edge_tp += per_sample_edge_tp
         self.curr_edge_fp += per_sample_edge_fp
         self.n_edge_samples += building.edge_corner_annots.shape[0]
         self.per_edge_sample_score.update({building._id: {'recall': per_sample_edge_tp/building.edge_corner_annots.shape[0], 'precision': per_sample_edge_tp/(per_sample_edge_tp+per_sample_edge_fp+1e-8)}}) 
 
-        statistics = np.array([per_sample_corner_tp, per_sample_corner_fp, gts.shape[0], per_sample_edge_tp, per_sample_edge_fp, building.edge_corner_annots.shape[0]])
-        return statistics
+        ## Compute loops precision/recall
+        per_sample_loop_tp = 0.0
+        per_sample_loop_fp = 0.0
 
-    # def _forward_edges(building, thresh=8.0):
+        pred_edge_map = draw_edges(building.edge_corner, building.corners_det, mode="det")
+        pred_edge_map = fill_regions(pred_edge_map)
+        annot_edge_map = draw_edges(building.edge_corner_annots, building.corners_annot, mode="annot")
+        annot_edge_map = fill_regions(annot_edge_map)
 
-    #     per_sample_tp = 0.0
-    #     per_sample_fp = 0.0
-    #     found = [False] * gts.shape[0]
+        pred_rs = extract_regions(pred_edge_map)
+        annot_rs = extract_regions(annot_edge_map)
 
-    #     for det in dets:
+        # for each predicted region
+        found = [False] * len(annot_rs)
+        for i, r_det in enumerate(pred_rs):
 
-    #         # get closest gt
-    #         near_gt = [0, 99999.0, (0.0, 0.0)]
-    #         for k, gt in enumerate(gts):
-    #             dist = np.linalg.norm(gt-det)
-    #             if dist < near_gt[1]:
-    #                 near_gt = [k, dist, gt] 
+            # get closest gt
+            near_gt = [0, 9999999.0, (0.0, 0.0)]
+            for k, r_gt in enumerate(annot_rs):
+                iou = np.logical_and(r_gt, r_det).sum()/np.logical_or(r_gt, r_det).sum()
+                if iou < near_gt[1]:
+                    near_gt = [k, iou, r_gt] 
 
-    #         # hit (<= threshold) and not found yet 
-    #         if near_gt[1] <= thresh and not found[near_gt[0]]:
-    #                 per_sample_tp += 1.0
-    #                 found[near_gt[0]] = True
+            # hit (<= thresh) and not found yet 
+            if near_gt[1] <= iou_thresh and not found[near_gt[0]]:
+                per_sample_loop_tp += 1.0
+                found[near_gt[0]] = True
 
-    #         # not hit or already found
-    #         else:
-    #             per_sample_fp += 1.0
+            # not hit or already found
+            else:
+                per_sample_loop_fp += 1.0
 
-    #     # update counters
-    #     self.curr_tp += per_sample_tp
-    #     self.curr_fp += per_sample_fp
-    #     self.n_samples += gts.shape[0]
-    #     self.per_sample_score.update({im_id: {'recall': per_sample_tp/gts.shape[0],
-    #                                    'precision': per_sample_tp/(per_sample_tp+per_sample_fp+1e-8)}}) 
-    # 	return
+        # update counters for corners
+        self.curr_loop_tp += per_sample_loop_tp
+        self.curr_loop_fp += per_sample_loop_fp
+        self.n_loop_samples += len(annot_rs)
+        self.per_loop_sample_score.update({building._id: {'recall': per_sample_loop_tp/len(annot_rs), 'precision': per_sample_loop_tp/(per_sample_loop_tp+per_sample_loop_fp+1e-8)}}) 
 
+        return 
+
+def extract_regions(region_mask):
+    inds = np.where((region_mask > 1) & (region_mask < 255))
+    tags = set(region_mask[inds])
+    tag_depth = dict()
+    rs = []
+    for t in tags:
+        if t > 0:
+            r = np.zeros_like(region_mask)
+            inds = np.where(region_mask == t)
+            r[inds[1], inds[0]] = 1
+            rs.append(r)
+    return rs
+
+def fill_regions(edge_mask):
+    edge_mask = edge_mask
+    tag = 2
+    for i in range(edge_mask.shape[0]):
+        for j in range(edge_mask.shape[1]):
+            if edge_mask[i, j] == 0:
+                edge_mask = _flood_fill(edge_mask, i, j, tag)
+                tag += 1
+    return edge_mask
+
+def _flood_fill(edge_mask, x0, y0, tag):
+    new_edge_mask = np.array(edge_mask)
+    nodes = [(x0, y0)]
+    new_edge_mask[x0, y0] = tag
+    while len(nodes) > 0:
+        x, y = nodes.pop(0)
+        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            if (0 <= x+dx < new_edge_mask.shape[0]) and (0 <= y+dy < new_edge_mask.shape[0]) and (new_edge_mask[x+dx, y+dy] == 0):
+                new_edge_mask[x+dx, y+dy] = tag
+                nodes.append((x+dx, y+dy))
+    return new_edge_mask
+
+def draw_edges(edge_corner, corners, mode="det"):
+
+    im = Image.new('L', (256, 256))
+    draw = ImageDraw.Draw(im)
+    for e in edge_corner:
+        c1, c2 = e
+        if "annot" in mode:
+            y1, x1, _, _ = corners[c1]
+            y2, x2, _, _ = corners[c2]
+        elif "det" in mode:
+            y1, x1 = corners[c1]
+            y2, x2 = corners[c2]
+        draw.line((x1, y1, x2, y2), width=1, fill='white')
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(im)
+    # plt.show(im)
+    return np.array(im)
