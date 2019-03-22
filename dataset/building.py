4cc1342d118f17e75c3970c43a48d1add5b5b746
csguestp <csguestp@cs-vml-gpu02.cs.sfu.ca>
2019-03-21 18:07:32 -0700

    mpnn

--- dataset/building.py
+++ dataset/building.py
@@ -256,7 +256,7 @@ class Building():
         self.edges_annot = edges_annot
 
         self.add_colinear_edges(self.edges_gt)
-        self.add_colinear_edges_annot()        
+        #self.add_colinear_edges_annot()        
         
         if options.suffix != '':
             suffix = '_' + corner_type + '_' + options.suffix
@@ -392,7 +392,6 @@ class Building():
         num_edges_source: source graph size, -1 for using the latest
         """
         #assert(num_edges_source < len(self.predicted_edges))
-        print(self._id)
         if self.with_augmentation:
             imgs, corners_det, edges_det = self.augment(self.rgb.copy(), self.corners_det, self.edges_det)
         else:
